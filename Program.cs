5d65cc8e97d4493b97ced3bbdd30e67ada475e01
ganiyevtemur1 <ganiyevtemur1@gmail.com>
2021-11-23 11:26:42 +0500

    .net 5.0 change

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+public class A{
+static void Main(string[] args){
 Console.WriteLine("Hello, World! jatemur.");
+}
+}
